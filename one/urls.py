8577cdc64c6482ee634bb449d50d5bd0a5102241
shwetasai <shwetasaini250796@gmail.com>
2025-04-08 14:23:55 +0530

    second db

---
+++ one/urls.py
@@ -0,0 +1,8 @@
+from one.views import (UserRegistrationView,LoginView)
+from django.urls import path
+from . import views
+
+urlpatterns =[
+    path('register/',UserRegistrationView.as_view(),name='register'),
+    path('login/',LoginView.as_view(),name='login'),
+]
