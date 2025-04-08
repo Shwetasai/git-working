5d63f91e2bd7a7e86dbd57b3d9213396a0254d9b
shwetasai <shwetasaini250796@gmail.com>
2025-04-08 14:24:58 +0530

    third db

---
+++ three/urls.py
@@ -0,0 +1,8 @@
+from three.views import (UserRegistrationView,LoginView)
+from django.urls import path
+from . import views
+
+urlpatterns =[
+    path('register/',UserRegistrationView.as_view(),name='register'),
+    path('login/',LoginView.as_view(),name='login'),
+]
